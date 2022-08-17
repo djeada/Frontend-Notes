3abce8ac9b7cb02ea1639a1ff3b86b61d2f59818
djeada <addjellouli1@gmail.com>
2022-08-17 18:59:23 +0200

    [feature]: embeding yt video example

--- projects/04_vanilla_js/yt_video/app.js
+++
@@ -1,8 +0,0 @@
-const navbarToggle = document.getElementById('navbarToggle');
-const navbarLinks = document.getElementById("navbar-links");
-
-navbarToggle.addEventListener('click', function() {
-    /*When the user presses the button, toggle between 
-    showing and hiding the navigation menu links. */
-    navbarLinks.style.display = navbarLinks.style.display === 'block' ? 'none' : 'block';
-});
