f61630768abd92f8e14289fb2bf5f053ce490249
djeada <addjellouli1@gmail.com>
2022-08-17 18:41:23 +0200

    [feature]: slide show example

--- projects/04_vanilla_js/slideshow/app.js
+++ projects/04_vanilla_js/slideshow/app.js
@@ -1,8 +1,26 @@
-const navbarToggle = document.getElementById('navbarToggle');
-const navbarLinks = document.getElementById("navbar-links");
-
-navbarToggle.addEventListener('click', function() {
-    /*When the user presses the button, toggle between 
-    showing and hiding the navigation menu links. */
-    navbarLinks.style.display = navbarLinks.style.display === 'block' ? 'none' : 'block';
-});
+var slideIndex = 1;
+        showSlides(slideIndex);
+        
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+        
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+        
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}    
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";  
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";  
+            dots[slideIndex-1].className += " active";
+        }
